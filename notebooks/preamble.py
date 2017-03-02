355f641fc59bd8b1a0150c1d6127955e2303b1b3
Andreas Mueller <t3kcit@gmail.com>
2017-03-02 15:32:56 -0800

    add preamble

---
+++ notebooks/preamble.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+import pandas as pd
+
+plt.rcParams['image.interpolation'] = "none"
+np.set_printoptions(precision=3)
+
+np, mglearn, pd
